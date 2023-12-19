945974d525fe75cf2d0d382d2d861b3a72c7e8fc
reijiro.ogura <reijiro820@gmail.com>
2023-12-19 12:28:22 +0900

    add:軽微な調整

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create show]
   resources :posts do
     resources :time_schedules, only: %i[new create destroy]
   end

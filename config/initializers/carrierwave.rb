f3a9c5360e6a02d30c109c1dacdb45811ffde1ca
reijiro.ogura <reijiro820@gmail.com>
2024-01-04 11:36:32 +0900

    権限の変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,6 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_public = false
     config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],

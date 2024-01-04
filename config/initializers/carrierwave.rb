73d5ea5e6dd1511dd75ecc6abbfeb359729e741d
reijiro.ogura <reijiro820@gmail.com>
2024-01-04 10:54:56 +0900

    fix carrierwave2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
     }
-    config.fog_directory  = "ENV['AWS_S3_BUCKET']"
+    config.fog_directory  = ENV['AWS_S3_BUCKET']
     config.cache_storage = :fog
   else
     config.storage = :file

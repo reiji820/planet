e409fd3d76a7f38774cca185990a2ae7f73401a1
reijiro.ogura <reijiro820@gmail.com>
2024-01-04 09:58:04 +0900

    carrierwaveto AWSの連携

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'sorcery'
 gem 'bootstrap', '~> 4.5.0'
 gem 'carrierwave', '~> 1.3', '>= 1.3.1'
+gem 'fog-aws'
 gem "mini_magick"
 gem 'active_hash'
 gem 'rails-i18n', '~> 6.0.0'
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,21 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_public = false
+    config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+    }
+    config.fog_directory  = "ENV['AWS_BUCKET_NAME']"
+    config.cache_storage = :fog
+  else
+    config.storage = :file
+  end
+end

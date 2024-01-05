a840d423bd29c661c57debc3887e1212c6af0214
reijiro.ogura <reijiro820@gmail.com>
2024-01-05 12:04:45 +0900

    add:編集画面からの遷移追加&Lint

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,12 +7,12 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
+      provider: 'AWS',
+      aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', nil),
+      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', nil),
+      region: 'ap-northeast-1'
     }
-    config.fog_directory  = ENV['AWS_S3_BUCKET']
+    config.fog_directory = ENV.fetch('AWS_S3_BUCKET', nil)
     config.cache_storage = :fog
   else
     config.storage = :file

31e023c43b952bd56c922d113b61df1b21098f25
reijiro.ogura <reijiro820@gmail.com>
2024-01-23 08:16:25 +0900

    プライバシーポリシーと利用規約の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/privacy'
   root to: 'posts#index'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  get 'privacy', to: 'pages#privacy'
+  get 'terms', to: 'pages#terms'
 
   resources :users, only: %i[new create show]
   resources :posts do

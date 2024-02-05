43c9af9beea3bd8cce97d372405dd81ea2c30888
reijiro.ogura <reijiro820@gmail.com>
2024-02-06 07:50:27 +0900

    投稿機能修正

--- app/javascript/packs/schedule_fields.js
+++ app/javascript/packs/schedule_fields.js
@@ -1,17 +1,13 @@
 document.addEventListener("turbolinks:load", () => {
-  // タイムスケジュールを追加する機能
   const addScheduleBtn = document.querySelector("#add_schedule");
   addScheduleBtn.addEventListener("click", () => {
     const timeSchedulesContainer = document.querySelector("#time-schedules");
-    // 現在時刻をミリ秒で取得してユニークなIDとして使用
-    const uniqueId = new Date().getTime();
-    const template = document.querySelector("#time-schedules-template").innerHTML.replace(/NEW_RECORD/g, uniqueId);
+    const template = document.querySelector("#time-schedules-template").innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
     // timeSchedulesContainerの最後にtemplateを挿入
     timeSchedulesContainer.insertAdjacentHTML("beforeend", template);
   });
 
-  // 削除ボタンの機能を動的に追加された要素に対しても適用
-  document.querySelector("#time-schedules-container").addEventListener("click", (e) => {
+  document.querySelector("#time-schedules").addEventListener("click", (e) => {
     if (e.target.matches(".remove_schedule")) {
       e.preventDefault();
       e.target.closest(".nested-fields").remove();
